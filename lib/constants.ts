c5125f6ab183c88dff6617a0cc4e70fb42165006
MOBIN MOSTAFA <mobinmostafa920@gmail.com>
2025-01-16 23:28:45 +0600

    search add

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'E-Commerce Store'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Your one-stop shop for groceries and organic products'
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'https://next-ecommerce.vercel.app'   
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Your one-stop shop for groceries and organic products' 
