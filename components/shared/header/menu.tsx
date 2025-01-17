8f0b603bc40be659785464b513584e82e233cd6d
MOBIN MOSTAFA <mobinmostafa920@gmail.com>
2025-01-17 23:51:03 +0600

    carousal add

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -5,14 +5,14 @@ export default function Menu() {
     return(
         <div className="flex justify-end">
           <nav className="flex gap-3 w-full">
-             <Link href="/signin" className="header-button" >
+             <Link href="/signin" className="header-button flex items-center" >
                 <UserIcon className="w-8 h-8" />
                 <span className="font-bold">Sign in</span>
              </Link>
 
-             <Link href="/cart" className="header-button" >
+             <Link href="/cart" className="header-button flex items-center" >
                  <ShoppingCartIcon className="w-8 h-8" />
-                 <span className="font-bold">Cart</span>
+                 <span className="font-bold ms-1">Cart</span>
              </Link>
           </nav>
         </div>
