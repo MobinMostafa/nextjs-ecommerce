fcb85038b36a3354b0d5f9a237c12ce37fdd9a30
MOBIN MOSTAFA <mobinmostafa920@gmail.com>
2025-01-18 11:25:28 +0600

    db add

---
+++ lib/db/index.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const cashed = (global as any).mongoose || { conn: null, promise: null };
+
+
+export const dbConnect = async (
+    mongooseUrl = process.env.MONGODB_URI
+) => {
+   if (cashed.conn) return cashed.conn;
+
+   if (!mongooseUrl) throw new Error('MONGODB_URI is missing');
+
+   cashed.promise = cashed.promise || mongoose.connect(mongooseUrl);
+
+   cashed.conn = await cashed.promise;
+
+   return cashed.conn
+};
